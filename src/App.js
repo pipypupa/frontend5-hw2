99da240305aa6efe3734b924a590952074d90d48
pipypupa <zong8729@gmail.com>
2025-09-27 22:52:48 +0300

    hw-2

---
+++ src/App.js
@@ -0,0 +1,23 @@
+function App() {
+  const Greeting = ({ name }) => <h1>Привіт, {name}!</h1>;
+
+  const Message = ({ text }) => <p>{text}</p>;
+
+  const Button = ({ onClick }) => (
+    <button onClick={onClick}>Натисни мене</button>
+  );
+
+  const handleClick = () => {
+    console.log("Кнопку натиснуто!");
+  };
+
+  return (
+    <div>
+      <Greeting name="Андрій" />
+      <Message text="Що робиш?." />
+      <Button onClick={handleClick} />
+    </div>
+  );
+}
+
+export default App;
